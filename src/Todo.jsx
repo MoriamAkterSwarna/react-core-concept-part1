e282d0959e4a40dc8ad58bc7ac2b8a9216038647
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2025-03-03 16:39:24 +0600

    Code for Video 38-7 added

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -20,14 +20,59 @@
 
 //* Conditional Rendering Option 2
 
+// export default function Todo({ task, isDone }) {
+//   if (isDone) {
+//     return <li>Finished : {task}</li>;
+//   }
+//   return <li>Work on : {task}</li>;
+// }
+
+//* For Video 38-6 end here
+
+//* for Video 38-7 start here
+
+//? Conditional Rendering Option 3: ternary operator
+
+// export default function Todo({ task, isDone }) {
+//     return <li>{isDone ? 'Finished' : 'Work On'} : {task}</li>;
+//     }
+
+//? Conditional Rendering Option 4: logical && operator
+
+// export default function Todo({ task, isDone }) {
+//   return (
+//     <li>
+//       {task} {isDone && ": Done"}
+//     </li>
+//   );
+// }
+
+//? Conditional Rendering Option 5: logical || operator
+
+// export default function Todo({ task, isDone }) {
+//   return (
+//     <li>
+//       {task} {isDone || ": Do it now"}
+//     </li>
+//   );
+// }
+
+
+
+//? Conditional Rendering Option 6 : Using a variable 
+
+
+
 export default function Todo({ task, isDone }) {
-  if (isDone) {
-    return <li>Finished : {task}</li>;
-  }
-  return <li>Work on : {task}</li>;
+    let listItem;
+    if (isDone) {
+      listItem = <li>Finished : {task}</li>;
+    } else {
+      listItem = <li>Work on : {task}</li>;
+    }
+
+    return listItem;
 }
 
-//* For Video 39-6 end here
 
-
-
+//* for Video 38-7 end here
