2e955ad35e89600013541de9860f685556ea747b
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2025-03-03 15:33:32 +0600

    Code For Video 38-2 added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+//  Video 38-2 start here
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
+
+//  Video 38-2 end here
