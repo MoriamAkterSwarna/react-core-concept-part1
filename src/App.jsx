4bf88c68579b6c658df80ef72fd8af0db5eb27d6
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2025-03-03 15:43:26 +0600

    Code For Video 38-3 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,52 @@
-
 //  Video 38-2 start here
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-      
       <h1>Vite + React</h1>
-      
+
+      <Person />
+      <Person />
+      <Person />
+      <Student />
     </>
+  );
+}
+
+// Video 38-3 start here
+
+function Person() {
+  const age = 25;
+  const money = 100;
+  const person = { name: "Sakib", age: 22 };
+
+  return (
+    <h3>
+      I am
+      {/* a Person  */}
+      {person.name}
+      with age: {age + money}
+    </h3>
+  );
+}
+
+function Student() {
+  return  ( 
+  <div>
+    <h3>This is a student</h3>
+    <p>Class: </p>
+    <p>score: </p>
+  </div>
   )
 }
 
-export default App
+
+
+// Video 38-3 end here
+
+
+
+export default App;
 
 //  Video 38-2 end here
